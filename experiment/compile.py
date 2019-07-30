e147f9e18151e5cc8487d8c14cf2f444f762e386
Nelson Houillon <houillon.nelson@gmail.com>
2019-07-30 21:27:41 +0200

    continue experiment

--- experiment/compile.py
+++ experiment/compile.py
@@ -1,9 +1,11 @@
 import marshal
-import struct
-import time
+from datetime import datetime
 from os import stat
 from py_compile import compile
 
+'''
+TODO use processes to isolate memory space & SIGSEGV errors
+'''
 
 def input():
     return [1, 6, 3, 7, 5, 9, 8, 2, 4, 10]
@@ -24,38 +26,42 @@ genome_stat = stat(genome_path)
 
 # read bytecode
 with open(genome_path, 'rb') as genome_file:
-    magic_number = genome_file.read(4)
-    modification_timestamp = genome_file.read(4)
-    source_size = genome_file.read(4)
-    unknown_field = genome_file.read(4)
+    magic_number = int.from_bytes(genome_file.read(4), byteorder='little', signed=False)
+    modification_timestamp = int.from_bytes(genome_file.read(4), byteorder='little', signed=False)
+    source_size = int.from_bytes(genome_file.read(4), byteorder='little', signed=False)
     original_bytecode = genome_file.read()
 
-modification_time = time.asctime(time.localtime(struct.unpack('L', modification_timestamp)[0]))
+modification_time = datetime.fromtimestamp(modification_timestamp)
 original_code = marshal.loads(original_bytecode)
 original_result = execute(original_code, input())
 
 # inspect pyc
 print(f'compiled {code_path} ({code_stat.st_size} bytes) to {genome_path} ({genome_stat.st_size} bytes)')
-print(f'magic number:      0x{magic_number.hex()}')
+print(f'magic number:      {magic_number:02x}')
 print(f'modification time: {modification_time}')
-print(f'source size:       0x{source_size.hex()}')
-print(f'unknown field:     0x{unknown_field.hex()}')
+print(f'source size:       {source_size:02x}')
 print(f'input:             {input()}')
 print(f'original result:   {original_result}')
 print()
 
-for random_byte in range(0xff + 1):
-    print(f'random byte: 0x{random_byte:02x}')
+all_bytes = list(range(0xff + 1))
 
-    mutated_bytecode = bytearray(original_bytecode)
-    mutated_bytecode[0] = random_byte
+for byte_index in (0, 14):
+    original_byte = original_bytecode[byte_index]
+    print(f'byte index: {byte_index}, value: 0x{original_byte:02x}')
 
-    try:
-        mutated_code = marshal.loads(mutated_bytecode)
-        mutated_result = execute(mutated_code, input())
-        print(f'\tmutated result: {mutated_result}')
-        # TODO fitness: number of numbers in ascending order
-    except Exception as e:
-        print(f'\terror: {e}')
-        # TODO disasembly that tolerate fault
-        # TODO diff disasemblies
+    for random_byte in all_bytes:
+
+        mutated_bytecode = bytearray(original_bytecode)
+        mutated_bytecode[byte_index] = random_byte
+        print(f'\trandom byte: 0x{random_byte:02x} -> ', end='')
+        try:
+            mutated_code = marshal.loads(mutated_bytecode)
+            mutated_result = execute(mutated_code, input())
+            print(mutated_result)
+            # TODO fitness: number of numbers in ascending order
+            # TODO label the code with a note (same, better, worse, malformed)
+        except Exception as e:
+            print(e)
+            # TODO disasembly that tolerate fault
+            # TODO diff disasemblies
